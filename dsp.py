0a1e05e74050fdf1e9cda815e60e12d3e7ef5fa4
Julien MacBook Pro <julien.f2004@icloud.com>
2025-03-24 12:35:14 +0000

    adding filter code part

--- dsp.py
+++ dsp.py
@@ -1,6 +1,7 @@
 import librosa
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.signal
 
 # Loading the signal in librosa
 audio = r"/Users/julien/Desktop/ESILV/A3/Dorset/Rapports/DSP Group Project/coyotes.mp3"
@@ -44,4 +45,41 @@ plt.xlabel('Frequency (Hz)')
 plt.ylabel('Magnitude')
 plt.grid(True)
 plt.minorticks_on()
+plt.show()
+
+# Defining the cut rates
+lowcut = 500.0
+highcut = 2000.0
+
+nyquist = 0.5 * sampling_rate
+low = lowcut / nyquist
+high = highcut / nyquist
+
+# FIR filter designing
+b = scipy.signal.firwin(numtaps=1001, cutoff=[low, high], pass_zero=False)
+
+# Application of the filter to the signal (convolution)
+y_filtered = scipy.signal.lfilter(b, 1, signal)
+
+# Printing the signal before and after the filtering
+plt.figure(figsize=(10, 6))
+
+plt.subplot(2, 1, 1)
+librosa.display.waveshow(signal, sr=sampling_rate)
+plt.title('Original Waveform')
+
+plt.subplot(2, 1, 2)
+librosa.display.waveshow(y_filtered, sr=sampling_rate)
+plt.title('Filtered Waveform')
+
+plt.tight_layout()
+plt.show()
+
+# Spectrogram visualisation after the filtering
+D_filtered = librosa.stft(y_filtered)
+S_db_filtered = librosa.amplitude_to_db(np.abs(D_filtered), ref=np.max)
+plt.figure(figsize=(10, 4))
+librosa.display.specshow(S_db_filtered, sr=sampling_rate, x_axis='time', y_axis='log')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Filtered Spectrogram')
 plt.show()
